47cd15bd0da33ef8ac377fe36a19058b92bdc97e
David Fecke <david@wunderbit.de>
2021-08-09 09:27:37 +0200

    feat: api endpoint for hash generation
    Dispatches blurhash generation by API.

---
+++ src/Controller/AdministrationController.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace EyeCook\BlurHash\Controller;
+
+use EyeCook\BlurHash\Message\GenerateHashMessage;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\Framework\Routing\Exception\InvalidRequestParameterException;
+use Shopware\Core\Framework\Routing\Exception\MissingRequestParameterException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Administrative Api endpoint
+ *
+ * @RouteScope(scopes={"api"})
+ *
+ * @package EyeCook\BlurHash
+ * @author David Fecke (+leptoquark1)
+ */
+class AdministrationController extends AbstractController
+{
+    protected MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    /**
+     * @Route(
+     *     "/api/_action/eyecook/blurhash/generate/media/{mediaId}",
+     *     name="api.ec.blurhash.generate.media-id",
+     *     defaults={"auth_required"=true},
+     *     methods={"GET"},
+     * )
+     */
+    public function generateByMediaEntity(?string $mediaId, Request $request, Context $context): JsonResponse
+    {
+        if (!$mediaId) {
+            throw new MissingRequestParameterException('mediaId');
+        }
+
+        $this->delegateHashMessage([$mediaId], $context);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Route(
+     *     "/api/_action/eyecook/blurhash/generate/media",
+     *     name="api.action.eyecook.blurhash.generate.media",
+     *     defaults={"auth_required"=true},
+     *     methods={"POST"}
+     * )
+     */
+    public function generateByMediaEntities(Request $request, Context $context): JsonResponse
+    {
+        if ($request->request->has('mediaIds') === false) {
+            throw new MissingRequestParameterException('mediaIds');
+        }
+
+        $mediaIds = $request->request->get('mediaIds');
+        if (is_array($mediaIds) === false) {
+            throw new InvalidRequestParameterException('mediaIds');
+        }
+
+        $this->delegateHashMessage($mediaIds, $context);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    private function delegateHashMessage(array $mediaIds, Context $context): void
+    {
+        $message = new GenerateHashMessage();
+        $message->setMediaIds($mediaIds);
+        $message->withContext($context);
+
+        $this->messageBus->dispatch($message);
+    }
+}
---
+++ tests/Controller/AdministrationControllerTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace EyeCook\BlurHash\Test\Controller;
+
+use Enqueue\Client\TraceableProducer;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Test\TestCaseBase\AdminFunctionalTestBehaviour;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+/**
+ * @package EyeCook\BlurHash\Test
+ * @author David Fecke (+leptoquark1)
+ */
+class AdministrationControllerTest extends TestCase
+{
+    use AdminFunctionalTestBehaviour;
+
+    protected Context $context;
+    protected TraceableProducer $traceableProducer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->traceableProducer = $this->getContainer()->get('enqueue.client.default.traceable_producer');
+        $this->context = Context::createDefaultContext();
+    }
+
+    public function testGenerateByMediaEntityReturnCode(): void
+    {
+        $this->getBrowser()->request('GET', '/api/_action/eyecook/blurhash/generate/media/' . Uuid::randomHex());
+        $response = $this->getBrowser()->getResponse();
+
+        static::assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testGenerateByMediaEntityMessageDispatched(): void
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testGenerateByMediaEntitiesReturnCode(): void
+    {
+        $this->getBrowser()->request('POST', '/api/_action/eyecook/blurhash/generate/media', [
+            'mediaIds' => [Uuid::randomHex()]
+        ]);
+        $response = $this->getBrowser()->getResponse();
+
+        static::assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testGenerateByMediaEntitiesMessageDispatch(): void
+    {
+        $this->markTestSkipped();
+    }
+}
