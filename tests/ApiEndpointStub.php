c3050072f9acdb1f673a852b874601cd380c3726
David Fecke <david@wunderbit.de>
2021-08-28 13:45:15 +0200

    test(chore): introduce stub for api-endpoint related tests
    - Default integration of `AdminFunctionalTestBehaviour` trait by shopware
    - New constraining method `fetch`, which will be essential for -integration-like- tests to provide small scaffolding footprints by return the parsed and assertion ready response body and corresponding response object.

---
+++ tests/ApiEndpointStub.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace EyeCook\BlurHash\Test;
+
+use Shopware\Core\Framework\Test\TestCaseBase\AdminFunctionalTestBehaviour;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @package EyeCook\BlurHash\Test
+ * @author David Fecke (+leptoquark1)
+ */
+trait ApiEndpointStub
+{
+    use AdminFunctionalTestBehaviour;
+
+    /**
+     * Usage:
+     * ```php
+     * ['response' => $response, 'content' => $content] = $this->getResponseResult($method, $url, $params);
+     *
+     * static::assertInstance(Response::class, $response);
+     * static::assert($expectedContent, $content);
+     * ```
+     *
+     * @return array{response: Response, content: ?string}
+     */
+    final protected function fetch(string $method, string $url, $parameters = []): array
+    {
+        $this->getBrowser()->request($method, $url, $parameters);
+        $response = $this->getBrowser()->getResponse();
+        try {
+            $content = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            $content = null;
+        }
+
+        return [
+            'response' => $response,
+            'content' => $content,
+        ];
+    }
+}
