a2ec5c67e99058694f354812ba6e5968081c3f02
David Fecke <david@wunderbit.de>
2021-08-28 14:30:33 +0200

    feat: manual-mode leverage handling
    Targets only Api-Scoped Controllers that may perform delegation for async hash encoding or related.
    
    - Prevent the message delegation when in manual-mode AND the admin-worker disabled
    - Add a trigger property in the `HashMessage` envelope
    - Provide an exception to be used when this condition applies
    - Extend `AdministrationController` to prevent manual mode leveration for hash message delegation performed by method `generateByMediaEntity` and `generateByMediaEntities`

---
+++ src/Controller/AbstractApiController.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace EyeCook\BlurHash\Controller;
+
+use EyeCook\BlurHash\Configuration\ConfigService;
+use EyeCook\BlurHash\Exception\IllegalManualModeLeverageException;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @RouteScope(scopes={"api"})
+ *
+ * @package EyeCook\BlurHash
+ * @author David Fecke (+leptoquark1)
+ */
+class AbstractApiController extends AbstractController
+{
+    private ?ConfigService $config = null;
+
+    /**
+     * @throws IllegalManualModeLeverageException
+     */
+    protected function preventManualModeLeverage(): void
+    {
+        if ($this->getConfig()->isAdminWorkerEnabled() === false && $this->getConfig()->isPluginManualMode()) {
+            throw new IllegalManualModeLeverageException();
+        }
+    }
+
+    protected function getConfig(): ConfigService
+    {
+        if ($this->config === null) {
+            $this->config = $this->container->get(ConfigService::class);
+        }
+
+        return $this->config;
+    }
+}
