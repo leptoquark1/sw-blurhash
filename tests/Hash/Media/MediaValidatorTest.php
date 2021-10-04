41dcf72a8837471e3e158654286fedf36dcfa64c
David Fecke <david@wunderbit.de>
2021-10-04 14:27:17 +0200

    test(hash): add incomplete test for `MediaValidator`

--- tests/Hash/Media/MediaValidatorTest.php
+++ tests/Hash/Media/MediaValidatorTest.php
@@ -2,12 +2,21 @@
 
 namespace Eyecook\Blurhash\Test\Hash\Media;
 
+use Eyecook\Blurhash\Configuration\Config;
 use Eyecook\Blurhash\Hash\Media\MediaValidator;
+use Eyecook\Blurhash\Test\ConfigMockStub;
 use Eyecook\Blurhash\Test\HashMediaFixtures;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Content\Media\MediaEntity;
+use Shopware\Core\Content\Media\MediaType\AudioType;
+use Shopware\Core\Content\Media\MediaType\BinaryType;
+use Shopware\Core\Content\Media\MediaType\DocumentType;
+use Shopware\Core\Content\Media\MediaType\VideoType;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\Tag\TagCollection;
+use Shopware\Core\System\Tag\TagEntity;
 
 /**
  * @package Eyecook\Blurhash\Test
@@ -16,6 +25,7 @@ use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 class MediaValidatorTest extends TestCase
 {
     use IntegrationTestBehaviour,
+        ConfigMockStub,
         HashMediaFixtures;
 
     protected Context $context;
@@ -24,6 +34,10 @@ class MediaValidatorTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->setUpSystemConfigService();
+        $this->resetInternalSystemConfigCache();
+        $this->resetInternalConfigCache();
 
         $this->context = Context::createDefaultContext();
         $this->mediaValidator = $this->getContainer()->get(MediaValidator::class);
@@ -42,27 +56,116 @@ class MediaValidatorTest extends TestCase
 
     public function testMediaWithoutFile(): void
     {
-        static::markTestIncomplete();
+        $mediaWithoutMimeType = $this->getValidLocalMediaForHash('jpg', null);
+        static::assertFalse($this->mediaValidator->validate($mediaWithoutMimeType));
+
+        $mediaWithoutExtension = $this->getValidLocalMediaForHash(null);
+        static::assertFalse($this->mediaValidator->validate($mediaWithoutExtension));
+
+        $mediaWithoutFilename = $this->getValidLocalMediaForHash('jpg', 'image/jpeg', ['fileName']);
+        static::assertFalse($this->mediaValidator->validate($mediaWithoutFilename));
+
+        $mediaAllInvalid = $this->getValidLocalMediaForHash(null, null, ['fileName']);
+        static::assertFalse($this->mediaValidator->validate($mediaAllInvalid));
     }
 
     public function testWrongMediaType(): void
     {
-        static::markTestIncomplete();
+        $mediaWithInvalidMediaTypeVideo = $this->getValidLocalMediaForHash();
+        $mediaWithInvalidMediaTypeVideo->setMediaType(new VideoType);
+
+        static::assertFalse($this->mediaValidator->validate($mediaWithInvalidMediaTypeVideo));
+
+        $mediaWithInvalidMediaTypeDocument = $this->getValidLocalMediaForHash();
+        $mediaWithInvalidMediaTypeDocument->setMediaType(new DocumentType);
+
+        static::assertFalse($this->mediaValidator->validate($mediaWithInvalidMediaTypeDocument));
+
+        $mediaWithInvalidMediaTypeBinary = $this->getValidLocalMediaForHash();
+        $mediaWithInvalidMediaTypeBinary->setMediaType(new BinaryType);
+
+        static::assertFalse($this->mediaValidator->validate($mediaWithInvalidMediaTypeBinary));
+
+        $mediaWithInvalidMediaTypeAudio = $this->getValidLocalMediaForHash();
+        $mediaWithInvalidMediaTypeAudio->setMediaType(new AudioType);
+
+        static::assertFalse($this->mediaValidator->validate($mediaWithInvalidMediaTypeAudio));
     }
 
-    public function testPrivateMedia(): void
+    public function testPrivateMediaNotAllowed(): void
     {
-        static::markTestIncomplete();
+        $this->setSystemConfigMock(Config::PATH_INCLUDE_PRIVATE, false);
+
+        $mediaPrivateTrue = $this->getValidLocalMediaForHash();
+        $mediaPrivateTrue->setPrivate(true);
+        static::assertFalse($this->mediaValidator->validate($mediaPrivateTrue));
+
+        $mediaPrivateFalse = $this->getValidLocalMediaForHash();
+        $mediaPrivateFalse->setPrivate(false);
+        static::assertTrue($this->mediaValidator->validate($mediaPrivateFalse));
+    }
+
+    public function testPrivateMediaAllowed(): void
+    {
+        $this->setSystemConfigMock(Config::PATH_INCLUDE_PRIVATE, true);
+
+        $mediaPrivateTrue = $this->getValidLocalMediaForHash();
+        $mediaPrivateTrue->setPrivate(true);
+        static::assertTrue($this->mediaValidator->validate($mediaPrivateTrue));
+
+        $mediaPrivateFalse = $this->getValidLocalMediaForHash();
+        $mediaPrivateFalse->setPrivate(false);
+        static::assertTrue($this->mediaValidator->validate($mediaPrivateFalse));
     }
 
     public function testMediaExcludedByFolder(): void
     {
-        static::markTestIncomplete();
+        $excludedFolderId = Uuid::randomHex();
+        $this->setSystemConfigMock(Config::PATH_EXCLUDED_FOLDERS, [$excludedFolderId]);
+
+        $mediaExcluded = $this->getValidLocalMediaForHash();
+        $mediaExcluded->setMediaFolderId($excludedFolderId);
+
+        static::assertFalse($this->mediaValidator->validate($mediaExcluded));
+    }
+
+    public function testMediaNotExcludedByFolder(): void
+    {
+        $excludedFolderId = Uuid::randomHex();
+        $this->setSystemConfigMock(Config::PATH_EXCLUDED_FOLDERS, [$excludedFolderId]);
+
+        $mediaExcluded = $this->getValidLocalMediaForHash();
+        $mediaExcluded->setMediaFolderId(Uuid::randomHex());
+
+        static::assertTrue($this->mediaValidator->validate($mediaExcluded));
     }
 
     public function testMediaExcludedByTag(): void
     {
-        static::markTestIncomplete();
+        $excludedTagId = Uuid::randomHex();
+        $tag = new TagEntity();
+        $tag->setId($excludedTagId);
+
+        $this->setSystemConfigMock(Config::PATH_EXCLUDED_TAGS, [$excludedTagId]);
+
+        $mediaExcluded = $this->getValidLocalMediaForHash();
+        $mediaExcluded->setTags(new TagCollection([$tag]));
+
+        static::assertFalse($this->mediaValidator->validate($mediaExcluded));
+    }
+
+    public function testMediaNotExcludedByTag(): void
+    {
+        $tag = new TagEntity();
+        $tag->setId(Uuid::randomHex());
+
+        $excludedTagId = Uuid::randomHex();
+        $this->setSystemConfigMock(Config::PATH_EXCLUDED_TAGS, [$excludedTagId]);
+
+        $mediaExcluded = $this->getValidLocalMediaForHash();
+        $mediaExcluded->setTags(new TagCollection([$tag]));
+
+        static::assertTrue($this->mediaValidator->validate($mediaExcluded));
     }
 
     public function testMediaFileExtensions(): void
