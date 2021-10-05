3e09e5c4b919491627249b76c4c7a6a13b0d8bbf
David Fecke <david@wunderbit.de>
2021-10-05 12:45:37 +0200

    test(command): add output tests for process command

--- tests/Command/ProcessCommandTest.php
+++ tests/Command/ProcessCommandTest.php
@@ -3,8 +3,14 @@
 namespace Eyecook\Blurhash\Test\Command;
 
 use Eyecook\Blurhash\Command\GenerateCommand;
+use Eyecook\Blurhash\Configuration\Config;
+use Eyecook\Blurhash\Test\ConfigMockStub;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Tester\CommandTester;
 
 /**
  * @package Eyecook\Blurhash\Test
@@ -12,18 +18,118 @@ use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
  */
 class ProcessCommandTest extends TestCase
 {
-    use IntegrationTestBehaviour;
+    use IntegrationTestBehaviour, ConfigMockStub;
 
     protected ?GenerateCommand $command = null;
 
     protected function setUp(): void
     {
-        parent::setUp();
+        $this->setUpSystemConfigService();
+        $this->resetInternalSystemConfigCache();
+        $this->resetInternalConfigCache();
+
+        $this->unsetSystemConfigMock(Config::PATH_MANUAL_MODE);
 
         $this->command = $this->getContainer()->get(GenerateCommand::class);
     }
 
-    public function testProcessCommand(): void
+    public function testIsAvailableByCommand(): void
+    {
+        $kernel = $this->getKernel();
+        $application = new Application($kernel);
+
+        $exception = null;
+        try {
+            // Thanks to shopware's `IntegrationTestBehaviour` we can not presume that no assertions were made
+            // So we need this workaround
+            $application->find('ec:blurhash:generate');
+        } catch (CommandNotFoundException $e) {
+            $exception = $e;
+        }
+
+        static::assertNull($exception, 'An CommandNotFoundException is thrown while try to find the command by name.');
+    }
+
+    public function testOutputWithAllFlag(): void
+    {
+        $tester = new CommandTester($this->command);
+
+        $tester->execute(['--all' => 1]);
+        $output = $tester->getDisplay(true);
+        static::assertStringContainsStringIgnoringCase('Existing hashes will be refreshed', $output);
+
+        $tester->execute([]);
+        $output = $tester->getDisplay(true);
+        static::assertStringContainsStringIgnoringCase('Generate missing hashes', $output);
+    }
+
+    public function testOutputWithDryRunFlag(): void
+    {
+        $tester = new CommandTester($this->command);
+
+        $tester->execute(['--dryRun' => 1]);
+        $output = $tester->getDisplay(true);
+
+        static::assertStringContainsStringIgnoringCase('media entities can be processed', $output);
+        static::assertStringNotContainsStringIgnoringCase('generation will be synchronous', $output);
+        static::assertStringNotContainsStringIgnoringCase('generation will be asynchronous', $output);
+    }
+
+    public function testOutputWithSyncFlag(): void
+    {
+        $tester = new CommandTester($this->command);
+
+        $tester->execute(['--sync' => 1]);
+        $output = $tester->getDisplay(true);
+        static::assertStringContainsStringIgnoringCase('generation will be synchronous', $output);
+
+        $tester->execute([]);
+        $output = $tester->getDisplay(true);
+        static::assertStringContainsStringIgnoringCase('generation will be asynchronous', $output);
+    }
+
+    public function testOutputInManualMode(): void
+    {
+        $this->setSystemConfigMock(Config::PATH_MANUAL_MODE, true);
+        $tester = new CommandTester($this->command);
+
+        $tester->execute(['--sync' => 1]);
+        $output = $tester->getDisplay(true);
+        static::assertStringNotContainsStringIgnoringCase(
+            'when plugin running in manual mode, asynchronous generation is disabled',
+            $output
+        );
+
+        $this->setSystemConfigMock(Config::PATH_MANUAL_MODE, false);
+        $tester->execute([]);
+        $output = $tester->getDisplay(true);
+        static::assertStringNotContainsStringIgnoringCase(
+            'when plugin running in manual mode, asynchronous generation is disabled',
+            $output
+        );
+
+        $this->setSystemConfigMock(Config::PATH_MANUAL_MODE, true);
+        $tester->execute([]);
+        $output = $tester->getDisplay(true);
+        static::assertStringContainsStringIgnoringCase(
+            'when plugin running in manual mode, asynchronous generation is disabled',
+            $output
+        );
+    }
+
+    public function testExitOnUnknownEntity(): void
+    {
+        $tester = new CommandTester($this->command);
+
+        $nonExistingEntity = Uuid::randomHex();
+
+        $tester->execute(['entities' => [$nonExistingEntity]]);
+        $output = $tester->getDisplay(true);
+
+        static::assertStringContainsStringIgnoringCase('Unknown entity "' . $nonExistingEntity . '"', $output);
+    }
+
+    public function testProcessCommandIntegration(): void
     {
         static::markTestIncomplete();
     }
