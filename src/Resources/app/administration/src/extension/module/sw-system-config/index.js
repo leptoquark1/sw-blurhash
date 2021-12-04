61f41542bc9396b068340f229d06b37da898613d
David Fecke <david@wunderbit.de>
2021-12-04 11:00:47 +0100

    fix(administration): plugin settings component extension

--- src/Resources/app/administration/src/extension/module/sw-system-config/index.js
+++ src/Resources/app/administration/src/extension/module/sw-system-config/index.js
@@ -1,10 +1,18 @@
-Shopware.Component.override('sw-system-config', {
+const { Component } = Shopware;
 
+/**
+ * As long as the configuration is of global scope, this component must be extended.
+ * This is currently the only way to change the property "salesChannelSwitchable" and
+ * thus hide the SalesChannel switch.
+ *
+ * This will produce a Vue warning
+ */
+Component.override('sw-system-config', {
   computed: {
     salesChannelSwitchable() {
-     return this.domain === 'EyecookBlurhash.config'
-       ? false
-       : this.$props.salesChannelSwitchable;
+      return this.domain === 'EyecookBlurhash.config'
+        ? false
+        : this.$props.salesChannelSwitchable;
     },
   },
 });
