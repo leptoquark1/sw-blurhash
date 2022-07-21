2a1992eef2a843a33fc102384e27a17b11c033e9
David Fecke <david@wunderbit.de>
2022-07-21 18:45:43 +0200

    chore(dev-ops): fix phpunit script in docker container

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -7,4 +7,4 @@ export XDEBUG_MODE=off
 composer dump-autoload -d "$WORK_DIR" --dev
 touch "$WORK_DIR/vendor/composer/InstalledVersions.php"
 
-php -n "$SW_DIR/vendor/bin/phpunit" -c "$WORK_DIR/phpunit.xml.dist" --bootstrap "$WORK_DIR/tests/TestBootstrap.php" "$@"
+php "$SW_DIR/vendor/bin/phpunit" -c "$WORK_DIR/phpunit.xml.dist" --bootstrap "$WORK_DIR/tests/TestBootstrap.php" "$@"
